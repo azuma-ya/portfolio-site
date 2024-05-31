f972016f71004130d49aa094610436bcda367a63
azuma-ya <kazu.bluemountain@gmail.com>
2024-06-01 03:31:07 +0900

    fix: eslintの追加

--- app/_components/layouts/BackGround.tsx
+++ app/_components/layouts/BackGround.tsx
@@ -6,6 +6,7 @@ const BackGround = () => {
   const distance = 1000;
   return (
     <Parallaxlayout
+      // eslint-disable-next-line
       className={`h-[calc(100%_+_${distance}px)] fixed inset-y-0 left-0 -z-10 w-full bg-hero-pattern bg-cover bg-repeat-y`}
       distance={distance}
     />

392a5a6a05393443670d4157a13a1e72f197ae96
azuma-ya <kazu.bluemountain@gmail.com>
2024-06-01 03:40:36 +0900

    fix: 背景画像の高さを修正する

--- app/_components/layouts/BackGround.tsx
+++ app/_components/layouts/BackGround.tsx
@@ -6,8 +6,9 @@ const BackGround = () => {
   const distance = 1000;
   return (
     <Parallaxlayout
-      // eslint-disable-next-line
-      className={`h-[calc(100%_+_${distance}px)] fixed inset-y-0 left-0 -z-10 w-full bg-hero-pattern bg-cover bg-repeat-y`}
+      className={
+        "fixed inset-y-0 left-0 -z-10 h-[calc(100%_+_1000px)] w-full bg-hero-pattern bg-cover bg-repeat-y"
+      }
       distance={distance}
     />
   );
